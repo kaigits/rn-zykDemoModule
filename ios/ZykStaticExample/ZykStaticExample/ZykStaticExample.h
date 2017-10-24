fac8e66a95714e78becfc35b93ef4df9aac2b52b
zhangyoukai <mac@mac.mac>
2017-10-24 17:10:52 +0800

    v1

---
+++ ios/ZykStaticExample/ZykStaticExample/ZykStaticExample.h
@@ -0,0 +1,13 @@
+//
+//  ZykStaticExample.h
+//  ZykStaticExample
+//
+//  Created by apple on 2017/10/24.
+//  Copyright © 2017年 zyktom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+@interface ZykStaticExample : NSObject<RCTBridgeModule>
+
+@end
---
+++ ios/ZykStaticExample/ZykStaticExample/ZykStaticExample.m
@@ -0,0 +1,69 @@
+//
+//  ZykStaticExample.m
+//  ZykStaticExample
+//
+//  Created by apple on 2017/10/24.
+//  Copyright © 2017年 zyktom. All rights reserved.
+//
+
+#import "ZykStaticExample.h"
+
+#import "RCTLog.h"
+#import "RCTEventDispatcher.h"
+
+static NSString * const kTestNotificationName = @"kTestNotificationName";
+static NSString * const TestEventName = @"TestEventName";
+@implementation ZykStaticExample
+
+- (instancetype)init {
+    if(self = [super init]) {
+        [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(sendEventToJS) userInfo:nil repeats:YES];
+    }
+    return self;
+}
+
+//此处不能使用OC的字符串，直接输入就行了
+RCT_EXPORT_MODULE(ZykStaticExample);
+
+//测试方法导出
+RCT_EXPORT_METHOD(testPrint:(NSString *)name info:(NSDictionary *)info) {
+    RCTLogInfo(@"%@: %@", name, info);
+}
+
+//测试回调函数
+RCT_EXPORT_METHOD(getNativeClass:(RCTResponseSenderBlock)callback) {
+    callback(@[NSStringFromClass([self class])]);
+}
+
+//Promiss
+RCT_REMAP_METHOD(testRespondMethod,
+                 name:(NSString *)name
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject) {
+    if([self respondsToSelector:NSSelectorFromString(name)]) {
+        resolve(@YES);
+    }
+    else {
+        reject(@"-1001", @"not respond this method", nil);
+    }
+}
+
+//线程
+- (dispatch_queue_t)methodQueue {
+    return dispatch_queue_create("com.zykai.rntest", DISPATCH_QUEUE_SERIAL);
+}
+
+
+//导出常量
+- (NSDictionary *)constantsToExport {
+    return @{ @"ZYKModuleName" : @"ZykStaticExample",
+              TestEventName: TestEventName
+              };
+}
+
+//发送事件
+- (void)sendEventToJS {
+    [self.bridge.eventDispatcher sendAppEventWithName:TestEventName body:@{@"name": @"Jack"}];
+}
+
+@end
